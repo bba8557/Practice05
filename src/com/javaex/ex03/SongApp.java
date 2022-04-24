61db7e5555787a82245458f20785618ee9d1a251
bba8557 <bba8557@naver.com>
2022-04-24 23:21:17 +0900

    22.04.24 ex03 예상해보기

--- src/com/javaex/ex03/SongApp.java
+++ src/com/javaex/ex03/SongApp.java
@@ -4,7 +4,9 @@ public class SongApp {
 
 	public static void main(String[] args){
 		
+		Song s1 = new Song("좋은날", "아이유", "Real", "이민수", "2010", 3);
 		
+		System.out.println(s1.toString());
 	}
 
 }
