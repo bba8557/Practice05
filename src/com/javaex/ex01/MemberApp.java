e117699584ca71c07a1a6724ed1617e0f0c2378b
bba8557 <bba8557@naver.com>
2022-04-24 22:58:17 +0900

    22.04.24 ex01

--- src/com/javaex/ex01/MemberApp.java
+++ src/com/javaex/ex01/MemberApp.java
@@ -1,10 +1,23 @@
 package com.javaex.ex01;
 
+import com.javaex.ex02.Goods;
+
 public class MemberApp {
 
 	public static void main(String[] args) {
 
-		
+		Member jws = new Member("정우성", "jws", 50000);
+		Member yis = new Member("유재석", "yjs", 30000);
+		Member lhr = new Member("이효리", "lhr", 40000);
+
+		// System.out.println(jws.toString());
+		// System.out.println(yis.toString());
+		// System.out.println(lhr.toString());
+
+		jws.member();
+		yis.member();
+		lhr.member();
+
 	}
 
 }
--- src/com/javaex/ex02/GoodsApp.java
+++ src/com/javaex/ex02/GoodsApp.java
@@ -4,19 +4,8 @@ public class GoodsApp {
 
 	public static void main(String[] args) {
 		
-		Goods jws  = new Goods("정우성", "jws", 50000);
-		Goods yis  = new Goods("유재석", "yjs", 30000);
-		Goods lhr  = new Goods("이효리", "lhr", 40000);
-		
-		//System.out.println(jws.toString());
-		//System.out.println(yis.toString());
-	    //System.out.println(lhr.toString());
-	    
-	    jws.member();
-	    yis.member();
-	    lhr.member();
 	}
+		
+		
 
-}
-
-
+	
